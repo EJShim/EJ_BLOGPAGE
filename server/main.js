720e7179b387b884a5c4975ad4e21596602847e8
ejshim <soulrommel@gmail.com>
2017-08-09 20:51:01 +0900

    update

--- server/main.js
+++ server/main.js
@@ -1,60 +1,53 @@
 import express from 'express';
-// import path from 'path';
-// import morgan from 'morgan'; // HTTP REQUEST LOGGER
-// import bodyParser from 'body-parser'; // PARSE HTML BODY
-// import mongoose from 'mongoose';
-// import session from 'express-session';
-// import api from './routes';
+import path from 'path';
+
+
+import morgan from 'morgan'; // HTTP REQUEST LOGGER
+import bodyParser from 'body-parser'; // PARSE HTML BODY
+import mongoose from 'mongoose';
+import session from 'express-session';
+
+import api from './routes';
 
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 1337;
 
-// // MongoDB
-// // app.use(morgan('dev'));
-// app.use(bodyParser.json());
+// MongoDB
+// app.use(morgan('dev'));
+app.use(bodyParser.json());
 
-// /* mongodb connection */
-// const db = mongoose.connection;
-// db.on('error', console.error);
-// db.once('open', () => { console.log('Connected to mongodb server'); });
-// // mongoose.connect('mongodb://username:password@host:port/database=');
-// mongoose.connect('mongodb://localhost/codelab');
+/* mongodb connection */
+const db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', () => { console.log('Connected to mongodb server'); });
+// mongoose.connect('mongodb://username:password@host:port/database=');
+mongoose.connect('mongodb://localhost/codelab');
 
-// /* use session */
-// app.use(session({
-//     secret: 'CodeLab1$1$234',
-//     resave: false,
-//     saveUninitialized: true
-// }));
+/* use session */
+app.use(session({
+    secret: 'CodeLab1$1$234',
+    resave: false,
+    saveUninitialized: true
+}));
 
 
 //Main Page
-// app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
-// //Router
-// app.use('/api', api);
+//Router
+app.use('/api', api);
 
-// /* support client-side routing */
-// app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'public/index.html'));
-// });
-
-
-app.use('/', (req, res)=>{
-    res.end('ha');
+/* support client-side routing */
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
 });
-
-
-
 
 /* handle error */
 app.use(function(err, req, res, next) {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
-
-
 
 ///Open Server
 app.listen(port, () => {
