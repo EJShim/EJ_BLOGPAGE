13705faf1826987bc959b9a43d99fe89506e79cc
ejshim <soulrommel@gmail.com>
2017-08-09 22:56:42 +0900

    update

--- src/js/components/E_VideoLayer/index.js
+++ src/js/components/E_VideoLayer/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layer from 'grommet/components/Layer';
+import Box from 'grommet/components/Box';
 
 class E_VideoLayer extends React.Component{
     constructor(props){
@@ -9,15 +10,15 @@ class E_VideoLayer extends React.Component{
 
     render(){
         const sourceURL = "https://www.youtube.com/embed/" + this.props.videoID;
-        const width = window.innerWidth - 20;
-        const height = window.innerHeight - 50;
-
-        console.log(width, height)
-
-        return(
-            <Layer closer={true} flush={false} onClose={this.props.onClose}>
-                <iframe widht={width} height={height} src={sourceURL} frameborder="0" allowfullscreen />
+        const width = window.innerWidth - 30;
+        const height = window.innerHeight - 30; 
+        return(            
+            <Layer closer={true} flush={true} onClose={this.props.onClose}>   
+                <Box full='horizontal'>             
+                    <iframe widht={width} height={height} src={sourceURL} frameborder="0" allowfullscreen />                
+                </Box>
             </Layer>
+            
         );
     }
 
